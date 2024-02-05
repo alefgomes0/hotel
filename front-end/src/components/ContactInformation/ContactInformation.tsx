634b40579d1621ed31ebe1b2f3fa7ad16906dd51
unknown <119986066+alefgomes0@users.noreply.github.com>
2024-02-04 22:24:31 -0300

    Change form styles

--- front-end/src/components/ContactInformation/ContactInformation.tsx
+++ front-end/src/components/ContactInformation/ContactInformation.tsx
@@ -24,57 +24,61 @@ export const ContactInformation = () => {
   return (
     <form
       onSubmit={(e) => e.preventDefault()}
-      className="grid grid-cols-2 auto-rows-max gap-y-6"
+      className="grid grid-flow-row auto-rows-max gap-y-6"
     >
-      <input
-        {...register("firstName")}
-        type="text"
-        placeholder="John"
-        className="w-60 h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
-      />
-      {errors.firstName && (
-        <p className="text-red-500 text-xs pt-[-18px] pb-3">
-          {errors.firstName.message}
-        </p>
-      )}
-      <input
-        {...register("lastName")}
-        type="text"
-        placeholder="Doe"
-        className="w-60 h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
-      />
-      {errors.lastName && (
-        <p className="text-red-500 text-xs pt-[-18px] pb-3">
-          {errors.lastName.message}
-        </p>
-      )}
-      <input
-        {...register("email")}
-        type="email"
-        placeholder="johndoe@email.com"
-        className="w-60 h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
-      />
-      {errors.email && (
-        <p className="text-red-500 text-xs pt-[-18px] pb-3">
-          {errors.email.message}
-        </p>
-      )}
-      <input
-        {...register("address")}
-        type="text"
-        placeholder="123 Actual Street"
-        className="w-60 h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
-      />
-      {errors.address && (
-        <p className="text-red-500 text-xs pt-[-18px] pb-3">
-          {errors.address.message}
-        </p>
-      )}
+      <div className="flex items-center gap-x-6">
+        <input
+          {...register("firstName")}
+          type="text"
+          placeholder="John"
+          className="w-[300px] h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
+        />
+        {errors.firstName && (
+          <p className="text-red-500 text-xs pt-[-18px] pb-3">
+            {errors.firstName.message}
+          </p>
+        )}
+        <input
+          {...register("lastName")}
+          type="text"
+          placeholder="Doe"
+          className="w-[300px] h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
+        />
+        {errors.lastName && (
+          <p className="text-red-500 text-xs pt-[-18px] pb-3">
+            {errors.lastName.message}
+          </p>
+        )}
+      </div>
+      <div className="flex items-center gap-x-6">
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="johndoe@email.com"
+          className="w-[300px] h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
+        />
+        {errors.email && (
+          <p className="text-red-500 text-xs pt-[-18px] pb-3">
+            {errors.email.message}
+          </p>
+        )}
+        <input
+          {...register("address")}
+          type="text"
+          placeholder="123 Actual Street"
+          className="w-[300px] h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
+        />
+        {errors.address && (
+          <p className="text-red-500 text-xs pt-[-18px] pb-3">
+            {errors.address.message}
+          </p>
+        )}
+      </div>
       <input
         {...register("phone")}
         type="text"
         placeholder="432213-3213"
-        className="w-60 h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
+        className="w-[300px] h-10 pl-2 py-6 border-transparent outline-none focus:ring-2 focus:ring-teal-700 rounded-sm"
       />
       {errors.phone && (
         <p className="text-red-500 text-xs pt-[-18px] pb-3">
--- front-end/src/pages/ConfirmReservation/ConfirmReservation.tsx
+++ front-end/src/pages/ConfirmReservation/ConfirmReservation.tsx
@@ -3,9 +3,9 @@ import { YourStay } from "@/components/YourStay/YourStay";
 
 export const ConfirmReservation = () => {
   return (
-    <main className="grid grid-cols-[3fr_1fr] grid-rows-1 min-h-[calc(100svh-90px)] gap-x-6 text-gray-700 bg-gray-100 px-32 pt-12">
+    <main className="grid grid-cols-[3fr_1fr] grid-rows-2 min-h-[calc(100svh-90px)] gap-x-6 text-gray-700 bg-gray-100 px-32 pt-12">
       <section className="grid">
-        <h4 className="text-2xl font-thin">COMPLETE RESERVATION</h4>
+        <h4 className="text-2xl font-thin mb-6">COMPLETE RESERVATION</h4>
         <ContactInformation />
       </section>
       <YourStay />
