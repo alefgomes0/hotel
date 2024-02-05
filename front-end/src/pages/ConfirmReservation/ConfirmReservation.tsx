d9f097151bd5f002e538f363fa00403211ea5a47
unknown <119986066+alefgomes0@users.noreply.github.com>
2024-02-05 20:59:59 -0300

    Change image name

--- front-end/src/pages/ConfirmReservation/ConfirmReservation.tsx
+++ front-end/src/pages/ConfirmReservation/ConfirmReservation.tsx
@@ -3,13 +3,14 @@ import { YourStay } from "@/components/YourStay/YourStay";
 
 export const ConfirmReservation = () => {
   return (
-    <main className="relative grid grid-cols-[3fr_1fr] grid-rows-2 min-h-[calc(100svh-90px)] gap-x-6 text-gray-700 bg-gray-100 px-32 pt-12">
+    <main className="relative grid grid-cols-[3fr_1fr] grid-rows-[auto_1fr] min-h-[calc(100svh-90px)] gap-x-6 text-gray-700 bg-gray-100 px-32 pt-12">
+      <h4 className="text-3xl font-thin mb-6">COMPLETE RESERVATION</h4>
       <section className="grid z-[20] bg-white px-8 py-4">
-        <h4 className="text-2xl font-thin mb-6">COMPLETE RESERVATION</h4>
+        <h4 className="text-xl font-thin mb-1">CONTACT INFORMATION</h4>
         <ContactInformation />
       </section>
       <YourStay />
-      <div className="w-full h-full absolute top-[0%] left-[0%] bg-[url(/images/teste.jpg)] z-[10] opacity-[12%] pointer-events-none"></div>
+      <div className="w-full h-full absolute top-[0%] left-[0%] bg-[url(/images/bg-pattern.jpg)] z-[10] opacity-[12%] pointer-events-none"></div>
     </main>
   );
 };
