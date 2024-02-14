6b31350c5eed6d8dcd36a5912abb8b9230ab724f
unknown <119986066+alefgomes0@users.noreply.github.com>
2024-02-14 19:59:24 -0300

    Use traits on the PriceController

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -1,59 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-use App\Http\Traits\CalculatePrice;
+use App\Traits\CalculatePrice;
 
 class PriceController extends Controller
 {
     use CalculatePrice;
     
-    public function getPartialAmount(string $suite_info): JsonResponse
+    public function getPartialAmount(string $suiteInfo): JsonResponse
     {   
-        $suite_infoObject = json_decode(strip_tags($suite_info));
-        $id = $suite_infoObject->id;
-        $days_of_stay = $suite_infoObject->daysOfStay;
-        $TAX_RATE = 0.043;
-
-        $price_per_day = DB::table('room_type')
-        ->select('price_per_day')
-        ->join('rooms', 'room_type_id', '=', 'room_type.id')
-        ->where('rooms.id', '=', $id)
-        ->first();
-        
-        $partialAmount = $price_per_day->price_per_day * $days_of_stay;
-        $taxes = round($partialAmount * $TAX_RATE, 2);
-        $partialPlusTaxes = $partialAmount + $taxes;
-
+        $price = $this->calculatePartialPrice($suiteInfo);
         return response()->json([
-            'partialAmount' => $partialAmount,
-            'taxes' => $taxes,
-            'partialPlusTaxes' => $partialPlusTaxes
-        ], 200);
+            'partialAmount' => $price['partialAmount'],
+            'taxes' => $price['taxes'],
+            'partialPlusTaxes' => $price['partialPlusTaxes']
+        , 200]);
     }
 
-    public function getTotalAmount(string $suites_info)
+    public function getTotalAmount(string $suitesInfo)
     {
-        $suites_type_object = json_decode(strip_tags($suites_info));
-        $suite_types = $suites_type_object->suiteTypes;
-        $days_of_stay = $suites_type_object->daysOfStay;
-        $TAX_RATE = 0.043;
-        $totalAmount = 0;
-
-        foreach($suite_types as $suite_type) {
-            $suite_price = DB::table('room_type')
-            ->select('price_per_day')
-            ->where('type', '=', $suite_type)
-            ->first();
-        
-            $totalAmount += ($suite_price->price_per_day * $days_of_stay) + ($suite_price->price_per_day * $days_of_stay * $TAX_RATE);
-        }
-
+        $suites = $this->calculateTotalPrice($suitesInfo);
         return response()->json([
-            'totalAmount' => $totalAmount
+            'totalAmount' => $suites['totalAmount']
         ], 200);
     }
 }
---
+++ app/Traits/CalculatePrice.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Traits;
+use Illuminate\Support\Facades\DB;
+
+trait CalculatePrice
+{
+  public function calculatePartialPrice(string $suiteInfo): array
+  {     
+        $suiteInfoArray = json_decode(strip_tags($suiteInfo));
+        $id = $suiteInfoArray->id;
+        $daysOfStay = $suiteInfoArray->daysOfStay;
+        $TAX_RATE = 0.043;
+
+        $pricePerDay = DB::table('room_type')
+        ->select('price_per_day')
+        ->join('rooms', 'room_type_id', '=', 'room_type.id')
+        ->where('rooms.id', '=', $id)
+        ->first();
+        
+        $partialAmount = $pricePerDay->price_per_day * $daysOfStay;
+        $taxes = round($partialAmount * $TAX_RATE, 2);
+        $partialPlusTaxes = $partialAmount + $taxes;
+		
+		return [
+			'partialAmount' => $partialAmount, 
+			'taxes' => $taxes, 
+			'partialPlusTaxes' => $partialPlusTaxes
+		];
+  	}
+
+  public function calculateTotalPrice(string $suitesInfo): Array
+  {
+		$suitesTypeArray = json_decode(strip_tags($suitesInfo));
+		$suiteTypes = $suitesTypeArray->suiteTypes;
+		$daysOfStay = $suitesTypeArray->daysOfStay;
+		$TAX_RATE = 0.043;
+		$totalAmount = 0;
+
+		foreach($suiteTypes as $suiteType) {
+			$suitePrice = DB::table('room_type')
+			->select('price_per_day')
+			->where('type', '=', $suiteType)
+			->first();
+		
+			$totalAmount += ($suitePrice->price_per_day * $daysOfStay) + ($suitePrice->price_per_day * $daysOfStay * $TAX_RATE);
+		}
+
+		return [
+			'totalAmount' => $totalAmount
+		];
+  }
+}
