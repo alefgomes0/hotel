a57abffee44176b908767a241d55bad10f028283
alexandre <119986066+alefgomes0@users.noreply.github.com>
2024-02-17 00:22:08 -0300

    add shadcn

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -26,15 +26,10 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "slide-right": {
-          from: { transform: "translateX(0%)" },
-          to: { transform: " translateX(100%) " }
-        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "pulseFast": "pulse 1s linear infinite"
       },
     },
   },
