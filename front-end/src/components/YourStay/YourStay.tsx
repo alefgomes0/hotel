08e329877752c7e95f56045323ddfce81f401d1f
alexandre <119986066+alefgomes0@users.noreply.github.com>
2024-02-22 17:33:07 -0300

    Add the useWindowSize custom hook

--- front-end/src/components/YourStay/YourStay.tsx
+++ front-end/src/components/YourStay/YourStay.tsx
@@ -2,18 +2,28 @@ import { useGuestInfo } from "@/hooks/useGuestInfo";
 import { YourStayHeader } from "../YourStayHeader/YourStayHeader";
 import { YourStaySuites } from "../YourStaySuites/YourStaySuites";
 import { YourStayTotal } from "../YourStayFooter/YourStayTotal";
-
+import { ExpandIcon } from "../svg/ExpandIcon";
 
 export const YourStay = () => {
   const { numOfGuests } = useGuestInfo();
+  const isMobile = window.innerWidth < 1024;
+
   return (
-    <article className="static lg:sticky lg:top-[5%] row-start-1 row-end-2 col-start-1 col-end-2 lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:col-end-3 self-start flex flex-col lg:w-[400px] justify-center text-gray-700 border-[1px] border-gray-400 gap-4 px-3 xl:px-6 py-1.5 xl:py-3 mb-4 lg:mb-0 shadow-[0_2px_2px_0_rgba(0,0,0,0.2)]">
+    <article className="relative lg:sticky lg:top-[5%] row-start-1 row-end-2 col-start-1 col-end-2 lg:row-start-1 lg:row-end-3 lg:col-start-2 lg:col-end-3 self-start flex flex-col lg:w-[400px] justify-center text-gray-700 border-[1px] border-gray-400 gap-4 px-3 xl:px-6 py-1.5 xl:py-3 mb-4 lg:mb-0 shadow-[0_2px_2px_0_rgba(0,0,0,0.2)]">
       <YourStayHeader />
       {numOfGuests.map((suite, index) => {
         return <YourStaySuites key={index} index={index} suite={suite} />;
       })}
       <div className="h-[1px] bg-gray-400"></div>
       <YourStayTotal />
+      {isMobile && (
+        <div className="absolute top-0 right-0 flex items-center justify-items-end gap-x-1">
+          <p className="text-xs opacity-60">View details</p>
+          <button title="view stay details" aria-label="view stay details">
+            <ExpandIcon width={32} height={32} />
+          </button>
+        </div>
+      )}
     </article>
   );
 };
--- front-end/src/pages/SearchResults/SearchResults.tsx
+++ front-end/src/pages/SearchResults/SearchResults.tsx
@@ -1,4 +1,5 @@
 import { ChooseRoom } from "@/components/ChooseRoom/ChooseRoom";
+import { GenericErrorDisplayer } from "@/components/GenericErrorDisplayer/GenericErrorDisplayer";
 import { RoomDisplayerSkeleton } from "@/components/Skeletons/RoomDisplayerSkeleton";
 import { SearchResultsHeader } from "@/components/SearchResultsHeader/SearchResultsHeader";
 import { useFetchAvailableRooms } from "../../hooks/useFetchAvailableRooms";
@@ -8,7 +9,7 @@ import { useLocation } from "react-router-dom";
 import { YourStay } from "@/components/YourStay/YourStay";
 import { SuitesDisplayer } from "@/components/SuitesDisplayer/SuitesDisplayer";
 import { useGoToCheckout } from "@/hooks/useGoToCheckout";
-import { GenericErrorDisplayer } from "@/components/GenericErrorDisplayer/GenericErrorDisplayer";
+import { useWindowSize } from "@/hooks/useWindowSize";
 
 export const SearchResults = () => {
   const location = useLocation();
@@ -18,6 +19,7 @@ export const SearchResults = () => {
     useFetchAvailableRooms();
   console.log(data);
   useGoToCheckout(selectedSuiteIndex.selected.length);
+  useWindowSize();
 
   return (
     <main className="grid grid-cols-1 xl:grid-cols-[3fr_1fr] grid-rows-[auto_auto_1fr] xl:grid-rows-[auto_1fr] gap-x-6 min-h-[calc(100svh-90px)] bg-gray-100 px-4 xl:px-16 pt-12">
