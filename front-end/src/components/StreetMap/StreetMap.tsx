f1f20bcf82fd8cb00aa1a3b91a2fee99c3285c19
alexandre <119986066+alefgomes0@users.noreply.github.com>
2024-03-09 11:42:43 -0300

    Add more content to the StreetMap

--- front-end/src/components/StreetMap/StreetMap.tsx
+++ front-end/src/components/StreetMap/StreetMap.tsx
@@ -1,25 +1,27 @@
-import { MapLibreGL } from "maplibre-gl";
-import { NavigationControl } from "react-map-gl";
-import Map from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import Map from "react-map-gl/maplibre";
+import { Marker } from "react-map-gl";
+import { NavigationControl } from "react-map-gl";
 import { useFetchMaptilerKey } from "@/hooks/useFetchMaptilerKey";
 
 export const StreetMap = () => {
   const { data, error, isLoading } = useFetchMaptilerKey();
   const MAPTILER_KEY = data?.data.key;
+  const latitude = 40.7056013;
+  const longitude = -74.0087755;
 
   return (
     <Map
-      mapLib={MapLibreGL}
       initialViewState={{
-        longitude: 0,
-        latitude: 0,
-        zoom: 2,
+        latitude,
+        longitude,
+        zoom: 13,
       }}
       style={{ width: "100%", height: "50vh" }}
-      mapStyle={`https://api.maptiler.com/maps/basic-v2/style.json?key=${MAPTILER_KEY}`}
+      mapStyle={`https://api.maptiler.com/maps/openstreetmap/style.json?key=${MAPTILER_KEY}`}
     >
       <NavigationControl position="top-right" />
+      <Marker latitude={latitude} longitude={longitude} color="#0f766e" />
     </Map>
   );
 };
